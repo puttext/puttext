a599b54b854e8b65812df4ab9fe33d0dd118c68c
Stefano Radaelli <s.radaelli@itsgenius.eu>
2017-03-15 10:29:27 +0100

    db

--- ordiniSM/database/seeds/ProdottiTableSeeder.php
+++ ordiniSM/database/seeds/ProdottiTableSeeder.php
@@ -4,82 +4,246 @@ use Illuminate\Database\Seeder;
 
 class ProdottiTableSeeder extends Seeder
 {
+
     /**
-     * Run the database seeds.
+     * Auto generated seed file
      *
      * @return void
      */
     public function run()
     {
-        \DB::table('prodotti')->delete();
+        
 
+        \DB::table('prodotti')->delete();
+        
         \DB::table('prodotti')->insert(array (
+            0 => 
             array (
+                'id' => 1,
                 'ordine_id' => 0,
-            	'fornitore_id' => 1,
-            	'tipo'=>'pane',
-            	'sottotipo'=>'1-tipo1',
-            	'descrizione'=>'Tipo 1',
+                'fornitore_id' => 1,
+                'tipo' => 'pane',
+                'sottotipo' => '1-tipo1',
+                'descrizione' => 'Tipo 1',
                 'quantita' => 'pagnotta',
-                'prezzo_fornitore' => 3.68,
-            	'contributo_des'=>0.07,
-            	'contributo_sm'=>0.25	
+                'prezzo_fornitore' => 3.0,
+                'contributo_des' => 0.0,
+                'contributo_sm' => 0.0,
+                'created_at' => NULL,
+                'updated_at' => NULL,
             ),
+            1 => 
             array (
+                'id' => 2,
                 'ordine_id' => 0,
-            	'fornitore_id' => 1,
-            	'tipo'=>'pane',
-            	'sottotipo'=>'2-integrale',
-            	'descrizione'=>'2-integrale',
+                'fornitore_id' => 1,
+                'tipo' => 'pane',
+                'sottotipo' => '2-integrale',
+                'descrizione' => 'Integrale',
                 'quantita' => 'pagnotta',
-                'prezzo_fornitore' => 3.68,
-            	'contributo_des'=>0.07,
-            	'contributo_sm'=>0.25	
+                'prezzo_fornitore' => 3.0,
+                'contributo_des' => 0.0,
+                'contributo_sm' => 0.0,
+                'created_at' => NULL,
+                'updated_at' => NULL,
             ),
+            2 => 
             array (
+                'id' => 3,
                 'ordine_id' => 0,
-            	'fornitore_id' => 1,
-            	'tipo'=>'pane',
-            	'sottotipo'=>'3-segale',
-            	'descrizione'=>'3-segale',
+                'fornitore_id' => 1,
+                'tipo' => 'pane',
+                'sottotipo' => '3-segale',
+                'descrizione' => 'Segale',
                 'quantita' => 'pagnotta',
-                'prezzo_fornitore' => 3.68,
-            	'contributo_des'=>0.07,
-            	'contributo_sm'=>0.25	
+                'prezzo_fornitore' => 3.0,
+                'contributo_des' => 0.0,
+                'contributo_sm' => 0.0,
+                'created_at' => NULL,
+                'updated_at' => NULL,
             ),
+            3 => 
             array (
+                'id' => 4,
                 'ordine_id' => 0,
-            	'fornitore_id' => 3,
-            	'tipo'=>'pane',
-            	'sottotpo'=>'1-tipo1',
-            	'descrizione'=>'Tipo 1',
+                'fornitore_id' => 2,
+                'tipo' => 'pane',
+                'sottotipo' => '1-tipo1',
+                'descrizione' => 'Tipo 1',
                 'quantita' => 'pagnotta',
-                'prezzo_fornitore' => 4.16,
-            	'contributo_des'=>0.08,
-            	'contributo_sm'=>0.16	
+                'prezzo_fornitore' => 3.0,
+                'contributo_des' => 0.0,
+                'contributo_sm' => 0.0,
+                'created_at' => NULL,
+                'updated_at' => NULL,
             ),
+            4 => 
             array (
+                'id' => 5,
                 'ordine_id' => 0,
-            	'fornitore_id' => 3,
-            	'tipo'=>'pane',
-            	'sottotpo'=>'2-integrale',
-            	'descrizione'=>'2-integrale',
+                'fornitore_id' => 2,
+                'tipo' => 'pane',
+                'sottotipo' => '2-integrale',
+                'descrizione' => 'Integrale',
                 'quantita' => 'pagnotta',
-                'prezzo_fornitore' => 4.16,
-            	'contributo_des'=>0.08,
-            	'contributo_sm'=>0.16	
+                'prezzo_fornitore' => 3.0,
+                'contributo_des' => 0.0,
+                'contributo_sm' => 0.0,
+                'created_at' => NULL,
+                'updated_at' => NULL,
             ),
+            5 => 
             array (
+                'id' => 6,
                 'ordine_id' => 0,
-            	'fornitore_id' => 3,
-            	'tipo'=>'pane',
-            	'sottotpo'=>'3-segale',
-            	'descrizione'=>'3-segale',
+                'fornitore_id' => 2,
+                'tipo' => 'pane',
+                'sottotipo' => '3-segale',
+                'descrizione' => 'Segale',
                 'quantita' => 'pagnotta',
-                'prezzo_fornitore' => 4.16,
-            	'contributo_des'=>0.08,
-            	'contributo_sm'=>0.16	
+                'prezzo_fornitore' => 3.0,
+                'contributo_des' => 0.0,
+                'contributo_sm' => 0.0,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            6 => 
+            array (
+                'id' => 7,
+                'ordine_id' => 0,
+                'fornitore_id' => 3,
+                'tipo' => 'pane',
+                'sottotipo' => '1-tipo1',
+                'descrizione' => 'Tipo 1',
+                'quantita' => 'pagnotta',
+                'prezzo_fornitore' => 4.0,
+                'contributo_des' => 0.0,
+                'contributo_sm' => 0.0,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            7 => 
+            array (
+                'id' => 8,
+                'ordine_id' => 0,
+                'fornitore_id' => 3,
+                'tipo' => 'pane',
+                'sottotipo' => '2-integrale',
+                'descrizione' => 'Integrale',
+                'quantita' => 'pagnotta',
+                'prezzo_fornitore' => 4.0,
+                'contributo_des' => 0.0,
+                'contributo_sm' => 0.0,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            8 => 
+            array (
+                'id' => 9,
+                'ordine_id' => 0,
+                'fornitore_id' => 3,
+                'tipo' => 'pane',
+                'sottotipo' => '3-segale',
+                'descrizione' => 'Segale',
+                'quantita' => 'pagnotta',
+                'prezzo_fornitore' => 4.0,
+                'contributo_des' => 0.0,
+                'contributo_sm' => 0.0,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            9 => 
+            array (
+                'id' => 10,
+                'ordine_id' => 0,
+                'fornitore_id' => 4,
+                'tipo' => 'pane',
+                'sottotipo' => '1-tipo1',
+                'descrizione' => 'Tipo 1',
+                'quantita' => 'pagnotta',
+                'prezzo_fornitore' => 3.0,
+                'contributo_des' => 0.0,
+                'contributo_sm' => 0.0,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            10 => 
+            array (
+                'id' => 11,
+                'ordine_id' => 0,
+                'fornitore_id' => 4,
+                'tipo' => 'pane',
+                'sottotipo' => '2-integrale',
+                'descrizione' => 'Integrale',
+                'quantita' => 'pagnotta',
+                'prezzo_fornitore' => 3.0,
+                'contributo_des' => 0.0,
+                'contributo_sm' => 0.0,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            11 => 
+            array (
+                'id' => 12,
+                'ordine_id' => 0,
+                'fornitore_id' => 4,
+                'tipo' => 'pane',
+                'sottotipo' => '3-segale',
+                'descrizione' => 'Segale',
+                'quantita' => 'pagnotta',
+                'prezzo_fornitore' => 3.0,
+                'contributo_des' => 0.0,
+                'contributo_sm' => 0.0,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            12 => 
+            array (
+                'id' => 13,
+                'ordine_id' => 0,
+                'fornitore_id' => 5,
+                'tipo' => 'pane',
+                'sottotipo' => '1-tipo1',
+                'descrizione' => 'Tipo 1',
+                'quantita' => 'pagnotta',
+                'prezzo_fornitore' => 3.0,
+                'contributo_des' => 0.0,
+                'contributo_sm' => 0.0,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            13 => 
+            array (
+                'id' => 14,
+                'ordine_id' => 0,
+                'fornitore_id' => 5,
+                'tipo' => 'pane',
+                'sottotipo' => '2-integrale',
+                'descrizione' => 'Integrale',
+                'quantita' => 'pagnotta',
+                'prezzo_fornitore' => 3.0,
+                'contributo_des' => 0.0,
+                'contributo_sm' => 0.0,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            14 => 
+            array (
+                'id' => 15,
+                'ordine_id' => 0,
+                'fornitore_id' => 5,
+                'tipo' => 'pane',
+                'sottotipo' => '3-segale',
+                'descrizione' => 'Segale',
+                'quantita' => 'pagnotta',
+                'prezzo_fornitore' => 3.0,
+                'contributo_des' => 0.0,
+                'contributo_sm' => 0.0,
+                'created_at' => NULL,
+                'updated_at' => NULL,
             ),
         ));
+        
+        
     }
 }
